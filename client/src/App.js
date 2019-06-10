7f39becead750753e35f2db2e67efeed2b5b3fca
Seth Wolfe <synthseth@gmail.com>
2019-06-09 21:22:16 -0400

    add basic auth and page structure boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './MessageApp-logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./MessageApp-logo.svg";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+import WithAuth from "./hocs/WithAuth";
+import LobbyPage from "./pages/lobby";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <WithAuth>
+          <LobbyPage />
+        </WithAuth>
+      </Router>
     </div>
   );
 }
