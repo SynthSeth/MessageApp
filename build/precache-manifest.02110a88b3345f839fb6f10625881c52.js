33c2429c6d4ce1d88171ec4dcb8eda26416e26a8
Seth Wolfe <synthseth@gmail.com>
2019-06-16 13:44:01 -0400

    revamp codebase (add build script on dev)

---
+++ build/precache-manifest.02110a88b3345f839fb6f10625881c52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86b34724d7c48841b1b54a2747eea0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4878f15316d3a4ba769",
+    "url": "/static/css/main.f6ed41cc.chunk.css"
+  },
+  {
+    "revision": "d4a021875018c449547a",
+    "url": "/static/js/2.9bb6e60d.chunk.js"
+  },
+  {
+    "revision": "c4878f15316d3a4ba769",
+    "url": "/static/js/main.ca0cf03d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f23c8f1106f1827b686bceadfc5d6bed",
+    "url": "/static/media/MessageApp-icon.f23c8f11.svg"
+  },
+  {
+    "revision": "5f5a2b934ac6f7cda4ee706a19354300",
+    "url": "/static/media/MessageApp-logo.5f5a2b93.svg"
+  },
+  {
+    "revision": "071744416e2a30367929a1b4fa6af1d9",
+    "url": "/static/media/background.07174441.svg"
+  }
+]);
